6f343f5e988a10758498fc487830ab8a872e2e6f
BloxBanter <bloxbanter@gmail.com>
2025-07-12 02:28:28 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -61,6 +61,7 @@ local State = {
     lastDeploymentTimes = {},
     startingInventory = {},
     unitNameSet = {},
+    selectedPortals = {},
     
     -- Auto settings
     autoBossEventEnabled = false,
